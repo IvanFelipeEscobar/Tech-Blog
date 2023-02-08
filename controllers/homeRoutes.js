98df7b292dccaba0c9e156a79f20806be31a4080
Ivan Escobar <ivnescbr1989@gmail.com>
2023-02-08 14:14:05 -0500

    home routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -21,18 +21,65 @@ router.get(`/`, async (req, res) => {
                 }
             ]
         })
-        const postData = dbPostData.map(post => post.get({ plain: true }))
-        res.render(`main`, {postData,
-        loggedIn: req.session.loggedIn})
+        const posts = dbPostData.map(post => post.get({ plain: true }))
+        res.render(`home`, {posts,
+        login: req.session.login})
         
     } catch (err) {
         console.log(err);
         res.status(500).json(err);
     }
 })
-module.exports = router
-//login route
 
+//login route
+router.get(`/login`, (req, res) =>{
+    if(req.session.login){
+        res.redirect(`/`)
+        return
+    }
+    res.render(`login`)
+})
 //signup route
+router.get(`/signup`, (req, res) =>{
+    if(req.session.login){
+        res.redirect(`/`)
+        return
+    }
+    res.render(`signup`)
+})
 
 //single post route
+router.get(`/post/:id`, async (req, res) => {
+    try {
+        const dbPostData = await Post.findOne({
+            where: {
+                id: req.params.id
+            },
+            attributes: [`id`, `title`, `created_at`, `post_content`],
+            include: [
+                {
+                    model: Comment,
+                    attributes: [`id`, `comment_content`, `post_id`, `user_id`, `created_at`],
+                    include: {
+                        model: User,
+                        attributes: `name`
+                    }
+                },
+                {
+                    model: User,
+                    attributes: `name`
+                }
+            ]
+        })
+        const post = dbPostData.map(post => post.get({ plain: true }))
+        res.render(`singlePost`, {post,
+        login: req.session.login})
+        
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+})
+
+
+module.exports = router
